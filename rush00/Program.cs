2ed62d94f420d1e76929aee861d9235ce00e9d7a
Janelle Kassandra <jkassand@oa-c3.msk.21-school.ru>
2021-07-03 15:04:43 +0300

    empty

---
+++ rush00/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rush00
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
